e98009628c32be24e73d45f1adb813f8c5ee2259
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-24 21:20:57 +0300

    Task 4 completed

---
+++ src/main/kotlin/Lesson3/Lesson3_Task4.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson3
+
+fun task4() {
+    var from = "E2"
+    var to = "E4"
+    var moveNumber = 1
+    println("from $from to $to, move = $moveNumber")
+    from = "D2"
+    to = "D3"
+    moveNumber++
+    println("from $from to $to, move = $moveNumber")
+}
