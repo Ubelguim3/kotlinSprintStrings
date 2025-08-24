75f81919ca0798d9c34d8edc0c78c7f5b1dcd364
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-24 18:21:48 +0300

    Task 2 completed

---
+++ src/main/kotlin/Lesson3/Lesson3_Task2.kt
@@ -0,0 +1,10 @@
+package org.example.Lesson3
+
+fun task2(){
+    var name = "Андреева Татьяна Сергеевна"
+    var age = 20
+    println("ФИО: $name, Возраст: $age")
+    name = "Сидорова Татьяна Сергеевна"
+    age = 22
+    println("ФИО: $name, Возраст: $age")
+}
