9ee1606c719644e92e2ac5bb3c7235fa55451532
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-27 23:20:34 +0300

    KS-3-35 mistakes is fixed

--- src/main/kotlin/Lesson3/Lesson3_Task5.kt
+++ src/main/kotlin/Lesson3/Lesson3_Task5.kt
@@ -1,11 +1,6 @@
 fun task5() {
     val moves = "D2-D4;0"
     val result = moves.split("-",";")
-//    val to = moves.slice(3 .. 4)
-//    val move = moves.substring(6)
-//println(from)
-//    println(to)
-//    println(move)
     println(result)
 }
 
