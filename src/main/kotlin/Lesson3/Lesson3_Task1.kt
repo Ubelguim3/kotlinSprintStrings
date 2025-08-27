c67fed5f1d806d051a232fb3e70048817e9aa516
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-28 01:00:57 +0300

    KS-3-1 mistakes is fixed

--- src/main/kotlin/Lesson3/Lesson3_Task1.kt
+++ src/main/kotlin/Lesson3/Lesson3_Task1.kt
@@ -5,11 +5,11 @@ fun task1() {
     var name: String
     var greeting: String
     name = "Стас"
-    greeting = "Добрый день, "
-    println(greeting + name)
+    greeting = "Добрый день,"
+    println("$greeting $name")
     name = "Катя"
-    greeting = "Добрый вечер, "
-    println(greeting + name)
+    greeting = "Добрый вечер,"
+    println("$greeting $name")
 }
 
 fun main() {
