189cf221d5ae8cc85d324bb4e9a5196899760a7e
Ubelguim3 <sakrivoshein@mail.ru>
2025-08-27 19:00:26 +0300

    KS-3-5 task completed using function "split" and extra files deleted

--- src/main/kotlin/Lesson3/Lesson3_Task5.kt
+++ src/main/kotlin/Lesson3/Lesson3_Task5.kt
@@ -1,9 +1,14 @@
 fun task5() {
     val moves = "D2-D4;0"
-    val from = moves.substring(0,2)
-    val to = moves.slice(3 .. 4)
-    val move = moves.substring(6)
-println(from)
-    println(to)
-    println(move)
+    val result = moves.split("-",";")
+//    val to = moves.slice(3 .. 4)
+//    val move = moves.substring(6)
+//println(from)
+//    println(to)
+//    println(move)
+    println(result)
+}
+
+fun main(){
+    task5()
 }
